49f601fe5d9206232e6798c443fc1d580dd14cc0
Sam <mcmyler@gmail.com>
2017-05-05 09:24:40 -0500

    Test fixes

--- js/clock.js
+++ js/clock.js
@@ -24,7 +24,7 @@ var clock = function () {
   var breakText = +breakLength.textContent
 
   var setBreakCounterNum = function (txt) {
-    breakCounterNum = +txt * 60 * 1000
+    breakCounterNum = +txt * 60 * 1000 + 1000
   }
   setBreakCounterNum(breakText)
 
@@ -40,28 +40,52 @@ var clock = function () {
     setBreakCounterNum(breakText)
   })
 
+  var sessionCounterNum
   var sessionLength = document.getElementById('session-length')
   var sessionText = +sessionLength.textContent
+
+  var setSessionCounterNum = function (txt) {
+    sessionCounterNum = +txt * 60 * 1000
+  }
+  setSessionCounterNum(sessionText)
 
   var counter = document.getElementById('time-left')
   var counterNum
   var setCounterNum = function (txt) {
     counterNum = +txt * 60 * 1000
   }
-  setCounterNum(counter.textContent)
+  // setCounterNum(counter.textContent)
 
+  var setCounter = function (num) {
+    var date = new Date()
+    date.setTime(num)
+    var m = date.getMinutes()
+    var s = date.getSeconds()
+    var checkTime = function checkTime (i) {
+      if (i < 10) { i = '0' + i };  // add zero in front of numbers < 10
+      return i
+    }
+    m = checkTime(m)
+    s = checkTime(s)
+    counter.textContent = m + ':' + s
+  }
   els['session-decrement'].addEventListener('click', function (target) {
     sessionText = dec(sessionText)
     sessionLength.textContent = sessionText
-    counter.textContent = sessionText
-    setCounterNum(counter.textContent)
+    // counter.textContent = sessionText
+    // setCounterNum(counter.textContent)
+    setSessionCounterNum(sessionText)
+    setCounter(sessionCounterNum)
   })
   els['session-increment'].addEventListener('click', function () {
     sessionText = inc(sessionText)
     sessionLength.textContent = sessionText
     counter.textContent = sessionText
-    setCounterNum(counter.textContent)
+    // setCounterNum(counter.textContent)
+    setSessionCounterNum(sessionText)
+    setCounter(sessionCounterNum)
   })
+
 
   // var fillBackground = function (num) {
   //   var circle = document.getElementById('circle')
@@ -70,7 +94,6 @@ var clock = function () {
 
   var isStopped = true
   var alreadyStarted = false
-
 
   var counterCb = function (e) {
     // this.removeEventListener('click', counterCb)
@@ -81,42 +104,56 @@ var clock = function () {
       if (!isStopped) {
         var date = new Date()
         if (session) {
-          counterNum = counterNum - 1000
-          // fillBackground()
-          date.setTime(counterNum)
+          // counterNum = counterNum - 1000
+          // // fillBackground()
+          // date.setTime(counterNum)
+          sessionCounterNum = sessionCounterNum - 1000
+          date.setTime(sessionCounterNum)
+          els['timer-label'].textContent = 'Session'
         } else {
           breakCounterNum = breakCounterNum - 1000
           // fillBackground()
           date.setTime(breakCounterNum)
+          els['timer-label'].textContent = 'Break!'
         }
         var m = date.getMinutes()
         var s = date.getSeconds()
-        var checkTime = function checkTime (i) {
+        var checkTime = function checkTime(i) {
           if (i < 10) { i = '0' + i };  // add zero in front of numbers < 10
           return i
         }
         m = checkTime(m)
         s = checkTime(s)
         counter.textContent = m + ':' + s
-        if (session && counterNum <= 0) {
+        var playBeep = function () {
+          var beep = document.getElementById('beep')
+          // beep.play()
+        }
+        if (session && sessionCounterNum <= 0) {
           session = false
           clearInterval(interval)
+          playBeep()
           interval = setInterval(countDown, 1000)
           setBreakCounterNum(breakLength.textContent)
-          els['timer-label'].textContent = 'Break!'
+          // els['timer-label'].textContent = 'Break!'
         } else if (!session && breakCounterNum <= 0) {
           clearInterval(interval)
           session = true
-          els['timer-label'].textContent = 'Session'
-          setCounterNum(sessionLength.textContent)
+          playBeep()
+          // els['timer-label'].textContent = 'Session'
+          // setCounterNum(sessionLength.textContent)
+          setSessionCounterNum(sessionLength.textContent)
+          sessionCounterNum = sessionCounterNum + 1000
           interval = setInterval(countDown, 1000)
         }
-        console.log('counterNum: ' + counterNum)
+        // console.log('counterNum: ' + counterNum)
+        console.log('sessionCounterNum: ' + sessionCounterNum)
         console.log('breakCounterNum: ' + breakCounterNum)
       }
       els.reset.addEventListener('click', function (e) {
         e.preventDefault()
         clearInterval(interval)
+        isStopped = true
         // els.circle.addEventListener('click', counterCb)
       })
     }
@@ -145,8 +182,9 @@ var clock = function () {
     breakLength.textContent = breakText
     breakLength.nodeValue = breakText
     document.getElementById('timer-label').textContent = 'Session'
-    document.getElementById('time-left').textContent = sessionText
-    setCounterNum(counter.textContent)
+    document.getElementById('time-left').textContent = '25:00'
+    // setCounterNum(counter.textContent)
+    setSessionCounterNum(sessionText)
     setBreakCounterNum(breakText)
   })
 }
